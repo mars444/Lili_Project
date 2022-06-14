85a8f3849013d7b5d51b0585244023485fc04bc0
Petr <petromars444@gmail.com>
2022-06-14 21:48:52 +0300

    Добавлена поддержка css и scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const miniCss = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     output: {
@@ -22,21 +24,77 @@ module.exports = {
                 },
             },
             {
-                test: /\.css$/,
+                test: /\.(png|jpg|jpeg|gif|svg|svgz)(\?.+)?$/,
+                exclude: /src\/assets\/icons/,
                 use: [
                     {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
+                        loader: 'url-loader',
+                        options: {
+                            name: 'images/[name].[ext]',
+                            limit: 1,
+                        },
                     },
                 ],
             },
+            {
+                test: /\.(eot|ttf|woff|woff2|otf)(\?.+)?$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            name: 'fonts/[name].[ext]',
+                            limit: 1,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.css/,
+                use: [
+                    miniCss.loader,
+                    'css-loader',
+                    'postcss-loader',
+                ],
+                sideEffects: true
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                sideEffects: true,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.svg$/,
+                exclude: [/src\/assets\/images/, /node_modules/],
+                include: path.resolve(__dirname, 'src/assets/icons'), // /src\/icons/,
+                loader: 'svg-sprite-loader',
+                options: {
+                    symbolId: (filePath) => 'icon-' + path.basename(filePath, '.svg'), //https://github.com/JetBrains/svg-sprite-loader#configuration
+                },
+            },
+            // {
+            //   test: /\.svg$/,
+            //   exclude: /node_modules/,
+            //   include: path.resolve(__dirname, 'src/assets/images'), // /src\/icons/,
+            //   loader: 'url-loader',
+            // },
         ],
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: './src/index.html',
         }),
+        new MiniCssExtractPlugin(),
     ],
 };
